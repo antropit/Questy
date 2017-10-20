38642f8eb03129364d22b63e1c6829f7544d3bf9
Fry Antropit <Fry Antropit>
2017-10-20 22:03:03 +0300

    Добавьте файлы проекта.

---
+++ Questy/Questy/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Questy
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
